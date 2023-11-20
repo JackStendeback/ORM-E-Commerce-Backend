1ccfdcd4005ab8e2b9c9b689c203a4e5ef9c23f0
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-20 13:48:02 -0600

    extensive additions made to the final routes in the tag-routes folder. improved error messages and names for readability

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,16 +33,54 @@ router.get('/:id', async (req, res) => {
 });
 
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(201).json(newTag);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagId = req.params.id;
+    const updatedTag = await Tag.update(
+      {
+        tag_name: req.body.tag_name,
+      },
+      {
+        where: {
+          id: tagId,
+        },
+      }
+    );
+    if (updatedTag[0] === 0) {
+      res.status(404).json({ message: 'Tag with this id not found!' });
+      return;
+    }
+    res.json({ message: 'Tag updated successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagId = req.params.id;
+    const deletedTagCount = await Tag.destroy({
+      where: { id: tagId }
+    });
+    if (deletedTagCount === 0) {
+      res.status(404).json({ message: 'Tag with this id not found!' });
+      return;
+    }
+    res.status(200).json({ message: 'Tag deleted successfully' });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
+
 
 module.exports = router;
