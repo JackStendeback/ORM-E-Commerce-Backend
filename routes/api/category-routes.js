75ae8c01181002150ee434e1bf60f0429bea5b67
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-20 13:44:22 -0600

    finished up adding routes to category routes file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,16 +33,50 @@ router.get('/:id', async (req, res) => {
 });
 
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(201).json(newCategory);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+    const updatedCategory = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: categoryId,
+        },
+      }
+    );
+    res.json(updatedCategory);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryId = req.params.id;
+    const deletedCategory = await Category.destroy({
+      where: { id: categoryId }
+    });
+    if (!deletedCategory) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(deletedCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
+
 
 module.exports = router;
