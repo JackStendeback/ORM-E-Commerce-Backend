77adaa980ed84fe9d4d449c56691da89e8bdcab9
Jack Stendeback <jackstendeback@jacks-mbp.lan>
2023-11-20 13:46:15 -0600

    creating routes within tag-routes file, mostly gets and now on to the posts

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,15 +3,35 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tags = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag, as: 'tagged_products'}]
+    });
+    res.status(200).json(tags);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const tagId = req.params.id;
+    const tag = await Tag.findByPk(tagId, {
+      include: [{ model: Product, through: ProductTag, as: 'tagged_products'}]
+    });
+
+    if (!tag) {
+      res.status(404).json({ message: 'Tag with this id not found!' });
+      return;
+    }
+
+    res.status(200).json(tag);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
+
 
 router.post('/', (req, res) => {
   // create a new tag
